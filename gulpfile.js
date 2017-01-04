c662350689b6032d2d886b9462adc0e0ac6a3f0c
Travis Gorman <thefarbetween@me.com>
2017-01-04 10:45:15 -0600

    gulp task styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
+// gulp.task('styles', function() {
+// 	gulp.src('css/styles.css')
+// 		.pipe(autoprefixer())
+// 		.pipe(gulp.dest('build'))	
+// })
+
+// gulp.task('watch', function() {
+// 	gulp.watch('css/styles.css', ['styles'])
+// })
+
+gulp.task('styles',function() {
 	gulp.src('css/styles.css')
 		.pipe(autoprefixer())
-		.pipe(gulp.dest('build'))	
+		.pipe(gulp.dest('build'))
 })
-
-gulp.task('watch', function() {
-	gulp.watch('css/styles.css', ['styles'])
-	})
