fa16573c455e8df44bbb9f3d16568d8ae4c29997
Travis Gorman <thefarbetween@me.com>
2017-01-04 10:51:03 -0600

    basic styles- body, a, wrapper

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,6 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-// gulp.task('styles', function() {
-// 	gulp.src('css/styles.css')
-// 		.pipe(autoprefixer())
-// 		.pipe(gulp.dest('build'))	
-// })
-
-// gulp.task('watch', function() {
-// 	gulp.watch('css/styles.css', ['styles'])
-// })
-
 gulp.task('styles',function() {
 	gulp.src('css/styles.css')
 		.pipe(autoprefixer())
